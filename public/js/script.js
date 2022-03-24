78abffed31c4b7564231b96611bcf4bcc87fbf11
Laserjonge <axelderuiter@gmail.com>
2022-03-24 12:39:20 +0100

    user.scss pop-up menu update

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,19 @@ const signIn = document.getElementById('secondary');
 const section = document.querySelector('.options');
 const signUp = document.querySelector('.signup');
 
-console.log(signIn);
+// console.log(signIn);
 
-signIn.addEventListener('click', () => {
-    section.classList.add('move');
-    signUp.classList.add('move-signup');
-    console.log("nu moet de section moven");
-})
+// signIn.addEventListener('click', () => {
+//     section.classList.add('move');
+//     signUp.classList.add('move-signup');
+//     console.log("nu moet de section moven");
+// })
+
+////////////////////////
+// Filter pop-up menu //
+////////////////////////
+const settingsButton = document.querySelector(".settings-button");
+
+settingsButton.addEventListener("click", toggleMenu = () => document.querySelector(".pop-up-form").classList.toggle("pop-up-function"));
+
+// settingsButton.addEventListener("click", console.log("hi"));
