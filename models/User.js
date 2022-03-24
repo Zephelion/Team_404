44bcd21489755f33259f1a49168ce55a47c700dd
EvaZ7 <evazaadnoordijk@gmail.com>
2022-03-24 10:40:14 +0100

    installed multer

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    picture:{
+        type:String,
+        required:true
+    },
     email:{
         type:String,
         required:true
