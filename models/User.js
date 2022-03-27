2508a5d017a919a6536c86f935a62a3a4bcb3076
Zephelion <russellnumo@outlook.com>
2022-03-27 20:12:05 +0200

    email is unique

--- models/User.js
+++ models/User.js
@@ -20,7 +20,8 @@ const userSchema = new mongoose.Schema({
     // },
     email:{
         type:String,
-        required:true
+        required:true,
+        unique:true
     },
     password:{
         type:String,
