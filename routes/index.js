5d9f57c9778058bb6e6493e592a0582c441f0dee
Zephelion <russellnumo@outlook.com>
2022-03-27 01:51:09 +0100

    moved multer to routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,21 @@ const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
 const user = require('../controllers/user');
+const path = require('path');
+
+//multer invoegen
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../public/uploads") );
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + "-" + Date.now() + file.originalname);
+  },
+});
+
+//afbeeldingen kunnen toevoegen
+const upload = multer({ storage: storage});
 
 
 //Hier zet ik alle routes en zo zet ik ze weer naar een controller
@@ -29,7 +44,7 @@ router.get("*", (req, res) => {
 router.get('/filter', user.filter);
 
 
-router.post('/register', user.store);
+router.post('/register', upload.single("picture"), user.store);
 
 
 module.exports = router;
