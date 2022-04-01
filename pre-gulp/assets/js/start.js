10bf55fb89ac4167130a47d76d9dab99c4ec3b58
Laserjonge <axelderuiter@gmail.com>
2022-04-01 16:24:50 +0200

    finish gulp

---
+++ pre-gulp/assets/js/start.js
@@ -0,0 +1,9 @@
+const signIn = document.getElementById('secondary');
+const section = document.querySelector('.options');
+const signUp = document.querySelector('.signup');
+
+signIn.addEventListener('click', () => {
+    section.classList.add('move');
+    signUp.classList.add('move-signup');
+    console.log("nu moet de section moven");
+})
